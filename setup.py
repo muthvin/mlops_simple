4a42dcfebb52a0c76020d7025bb6732864b569a5
muthvin <39585434+muthvin@users.noreply.github.com>
2021-11-07 17:14:54 +0530

    updated

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='src', version="0.0.1",
+    description="Its a winequality package",
+    author="vinod",
+    packages=find_packages(),
+    license="MIT"
+)
